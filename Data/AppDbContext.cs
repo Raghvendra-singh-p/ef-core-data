83fbdc7e60930c4531ed453787c48d9e3f55a1b0
Raghvendra <raghvendra@viruspositive.com>
2023-12-16 18:04:11 +0530

    updates

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ef_core_data.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<DbContext> options): base(options) 
+        {
+            
+        }
+    }
+}
